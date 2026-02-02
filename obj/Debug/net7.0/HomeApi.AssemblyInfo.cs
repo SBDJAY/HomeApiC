4c31e632d2dc889cf9584f5e53bc77f74030f1b0
SBDJAY <sbdjay183@gmail.com>
2026-02-02 03:15:47 -0500

    This Comit shows taht using swagger with the port# works

--- obj/Debug/net7.0/HomeApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/HomeApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de1864bbbe47a31a23ee5a707263504387f6c07")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
